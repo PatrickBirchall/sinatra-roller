1095b1f1cc0d12f72ed5fe0a387796b3c8709d68
Patrick Birchall <pbirchall@equalexperts.com>
2020-05-20 09:57:54 +0100

    More dice

--- server.rb
+++ server.rb
@@ -9,7 +9,27 @@ get '/d20' do
   "Result: #{roll}"
 end
 
+get '/d12' do
+  roll = rand(12) +1
+  "Result: #{roll}"
+end
+
 get '/d10' do
   roll = rand(10) +1
+  "Result: #{roll}"
+end
+
+get '/d8' do
+  roll = rand(8) +1
+  "Result: #{roll}"
+end
+
+get '/d6' do
+  roll = rand(6) +1
+  "Result: #{roll}"
+end
+
+get '/d4' do
+  roll = rand(4) +1
   "Result: #{roll}"
 end
